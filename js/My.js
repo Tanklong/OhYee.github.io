faf3b43fb1ab9c9daf7d62c43e35ed5e91faca1f
OhYee <oyohyee@oyohyee.com>
2017-04-07 19:56:52 +0800

    Site updated: 2017-04-07 19:55:23

---
+++ js/My.js
@@ -0,0 +1,13 @@
+//document.write("<script language=javascript src='http://cdn.bootcss.com/jquery/1.11.1/jquery.min.js'></script>");
+document.write("<script language=javascript src='/js/jquery.js'></script>");
+document.write("<script language=javascript src='/js/after_jquery.js'></script>");
+
+
+
+//网站重定向
+host = window.location.host
+if(host=="ohyee.coding.me")
+    window.location.href = window.location.href.replace(/ohyee.coding.me/,"www.oyohyee.com")
+
+
+//alert("加载完成");
